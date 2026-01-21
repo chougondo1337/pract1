5b01230e2ab6e03c9ef6aa56d57a85be8fd2a9e2
chougondo1337 <6969suns3tacc@gmail.com>
2026-01-21 08:52:10 +0000

    2

--- aksk1.cpp
+++ aksk1.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "візитка:\tстудента:\tгрупи п24 " << std::endl;
     std::cout << "по пріколу\n" ;
-    std::cout << "зроблена "<< std::endl;
+    std::cout << "зроблена \n";
     return 0;
 }
